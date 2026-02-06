99eadb07eacef2e0e332a0efd200d14f86afdf37
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-02-06 22:15:16 +0600

    added problem-solving-4

---
+++ problem-solving-4.js
@@ -0,0 +1,34 @@
+function gonoVote(array) {
+
+  if (Array.isArray(array) === false) {
+    return "Invalid";
+  }
+
+  let haCount = 0;
+  let naCount = 0;
+
+  for (const vote of array) {
+    if (vote === "ha") {
+      haCount = haCount + 1;
+    }
+    else if (vote === "na") {
+      naCount = naCount + 1;
+    }
+  }
+
+  if (haCount > naCount) {
+    return true;
+  }
+  else if (haCount === naCount) {
+    return "equal";
+  } 
+  else {
+    return false;
+  }
+}
+
+console.log(gonoVote(["ha", "na", "ha", "na"]));
+console.log(gonoVote(["ha", "na", "na"]));
+console.log(gonoVote(["ha", "ha", "ha", "na"]));
+console.log(gonoVote("ha, na"));
+console.log(gonoVote(1234));
